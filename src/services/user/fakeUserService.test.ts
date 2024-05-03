084ba95b58d6c8dc449f0eaf4b5588f9a6a72e3f
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2024-05-03 11:44:53 +0800

    Create unit test for FakeUserService's createUser method.

---
+++ src/services/user/fakeUserService.test.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import * as fs from 'fs/promises';
+import { UserService } from './userService'; // Path to your cacheService file
+
+describe('FakeUserService', () => {
+	describe('createUser', () => {
+		it('should ', async () => {});
+	});
+});
