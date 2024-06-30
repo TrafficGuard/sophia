55627e6f6eeccdb8b0ad44f9f0ddac804de37d0e
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2024-06-30 21:56:30 +0800

    Compile fix

--- src/cli/chat.ts
+++ src/cli/chat.ts
@@ -1,23 +1,26 @@
 import { readFileSync, writeFileSync } from 'fs';
+import { LlmFunctions } from '#agent/LlmFunctions';
 import { AgentContext, AgentLLMs, agentContextStorage, createContext } from '#agent/agentContext';
-import { Toolbox } from '#agent/toolbox';
 import '#fastify/trace-init/trace-init';
 import { LLM } from '#llm/llm';
 import { Claude3_Opus, Claude3_Sonnet } from '#llm/models/anthropic';
-import { Claude3_Haiku_Vertex, Claude3_Opus_Vertex, Claude3_Sonnet_Vertex } from '#llm/models/anthropic-vertex';
+import { Claude3_5_Sonnet_Vertex, Claude3_Haiku_Vertex, Claude3_Opus_Vertex, Claude3_Sonnet_Vertex } from '#llm/models/anthropic-vertex';
 import { Gemini_1_5_Experimental, Gemini_1_5_Pro } from '#llm/models/vertexai';
 
+import { deepseekCoder } from '#llm/models/deepseek';
 import { GPT4o } from '#llm/models/openai';
 import { currentUser } from '#user/userService/userContext';
 
 // Usage:
 // npm run ai
 
-let llm: LLM = Claude3_Sonnet_Vertex();
+const llm: LLM = Claude3_5_Sonnet_Vertex();
 // llm = Claude3_Opus();
 // llm = Gemini_1_5_Pro();
 // llm = Gemini_1_5_Experimental();
-llm = Claude3_Haiku_Vertex();
+// llm = Claude3_Haiku_Vertex();
+// llm = Claude3_Opus_Vertex();
+// llm = deepseekCoder();
 
 const llms: AgentLLMs = {
 	easy: llm,
@@ -36,11 +39,11 @@ async function main() {
 		agentName: 'chat',
 		llms,
 		user: currentUser(),
-		toolbox: new Toolbox(),
+		functions: new LlmFunctions(),
 	});
 	agentContextStorage.enterWith(context);
 
-	const text = await llm.generateText(prompt);
+	const text = await llm.generateText(undefined, prompt);
 
 	writeFileSync('src/cli/chat-out', text);
 	console.log('wrote to chat-out');
