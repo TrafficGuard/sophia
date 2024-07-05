f5c666971b96a0d04fc92951b11e03840ce56a72
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2024-07-05 17:30:05 +0800

    compile fix

--- src/cli/chat.ts
+++ src/cli/chat.ts
@@ -3,24 +3,14 @@ import { LlmFunctions } from '#agent/LlmFunctions';
 import { AgentContext, AgentLLMs, agentContextStorage, createContext } from '#agent/agentContext';
 import '#fastify/trace-init/trace-init';
 import { LLM } from '#llm/llm';
-import { Claude3_Opus, Claude3_Sonnet } from '#llm/models/anthropic';
-import { Claude3_5_Sonnet_Vertex, Claude3_Haiku_Vertex, Claude3_Opus_Vertex, Claude3_Sonnet_Vertex } from '#llm/models/anthropic-vertex';
-import { Gemini_1_5_Experimental, Gemini_1_5_Pro } from '#llm/models/vertexai';
+import { Claude3_5_Sonnet_Vertex } from '#llm/models/anthropic-vertex';
 
-import { deepseekCoder } from '#llm/models/deepseek';
-import { GPT4o } from '#llm/models/openai';
 import { currentUser } from '#user/userService/userContext';
 
 // Usage:
-// npm run ai
+// npm run chat
 
 const llm: LLM = Claude3_5_Sonnet_Vertex();
-// llm = Claude3_Opus();
-// llm = Gemini_1_5_Pro();
-// llm = Gemini_1_5_Experimental();
-// llm = Claude3_Haiku_Vertex();
-// llm = Claude3_Opus_Vertex();
-// llm = deepseekCoder();
 
 const llms: AgentLLMs = {
 	easy: llm,
