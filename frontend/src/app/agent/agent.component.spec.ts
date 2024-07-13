fb644a8943cd62ba7b47f68b06ac692be46d7eff
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2024-07-13 15:41:17 +0800

    Fix frontend tests

--- frontend/src/app/agent/agent.component.spec.ts
+++ frontend/src/app/agent/agent.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AgentComponent } from './agent.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '@app/material.module';
 
 describe('AgentComponent', () => {
   let component: AgentComponent;
@@ -10,7 +12,7 @@ describe('AgentComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AgentComponent],
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule, ReactiveFormsModule, MaterialModule],
     }).compileComponents();
   });
 
--- frontend/src/app/agents/agents.component.spec.ts
+++ frontend/src/app/agents/agents.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AgentsComponent } from './agents.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '@app/material.module';
 
 describe('AgentsComponent', () => {
   let component: AgentsComponent;
@@ -8,6 +12,7 @@ describe('AgentsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule, ReactiveFormsModule, MaterialModule],
       declarations: [AgentsComponent],
     }).compileComponents();
 
