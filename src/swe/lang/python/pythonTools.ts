43e1e2e27f57c67361a43270fc061dba75934c89
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2024-08-27 11:02:18 +0800

    Update pythonTools.ts

--- src/swe/lang/python/pythonTools.ts
+++ src/swe/lang/python/pythonTools.ts
@@ -9,7 +9,7 @@ import {getPythonPath} from "#swe/codeEditor";
 export class PythonTools implements LanguageTools {
 	async generateProjectMap(): Promise<string> {
 		logger.info(getFileSystem().getWorkingDirectory());
-		const { stdout, stderr /*, exitCode*/ } = await execCmd(`${getPythonPath()} -m aider --show-repo-map`);
+		const { stdout, stderr /*, exitCode*/ } = await execCmd(`${getPythonPath()} -m aider --yes --map-tokens 2048 --show-repo-map`);
 		// stubgen --ignore-errors -o stubs
 
 		// if (exitCode > 0) throw new Error(`${stdout} ${stderr}`);
