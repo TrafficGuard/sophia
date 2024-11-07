22f7ebe23745918a586960a557afbe97a73959ff
danielcampagnolitg <138441775+danielcampagnolitg@users.noreply.github.com>
2024-11-07 12:20:39 +0800

    Fix Add agent button. Fix ui-settings routing

--- frontend/src/app/modules/profile/ui-settings/ui-settings.component.ts
+++ frontend/src/app/modules/profile/ui-settings/ui-settings.component.ts
@@ -33,76 +33,46 @@ export class UiSettingsComponent implements OnInit, OnDestroy {
     scheme: 'dark' | 'light';
     theme: string;
     themes: Themes;
-    private _unsubscribeAll: Subject<any> = new Subject<any>();
+    private readonly _unsubscribeAll = new Subject<any>();
 
-    /**
-     * Constructor
-     */
     constructor(
         private _router: Router,
         private _fuseConfigService: FuseConfigService,
         private _localStorageService: LocalStorageService
-    ) {
-        this._unsubscribeAll = new Subject();
-    }
+    ) {}
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks --------------------------------------
 
-    /**
-     * On init
-     */
     ngOnInit(): void {
         // Subscribe to config changes
         this._fuseConfigService.config$
             .pipe(takeUntil(this._unsubscribeAll))
             .subscribe((config: FuseConfig) => {
-                // Store the config
                 this.config = config;
             });
     }
 
-    /**
-     * On destroy
-     */
     ngOnDestroy(): void {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next(null);
         this._unsubscribeAll.complete();
     }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods --------------------------------------
 
     /**
      * Set the layout on the config
-     *
      * @param layout
      */
     setLayout(layout: string): void {
-        // Clear the 'layout' query param to allow layout changes
-        this._router
-            .navigate([], {
-                queryParams: {
-                    layout: null,
-                },
-                queryParamsHandling: 'merge',
-            })
-            .then(() => {
-                // Set the config
-                this._fuseConfigService.config = { layout };
-                this._localStorageService.setLayout(layout);
-            }).catch(console.error);
+        this._fuseConfigService.config = { layout };
+        this._localStorageService.setLayout(layout);
     }
 
     /**
      * Set the scheme on the config
-     *
      * @param scheme
      */
-
     setScheme(scheme: Scheme): void {
         this._fuseConfigService.config = { scheme };
         this._localStorageService.setScheme(scheme);
