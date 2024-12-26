9b4d1917b7b19c6bdfd15bae32ebc21f4ffa73a5
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2024-12-26 23:16:53 +0800

    Update web.test.ts

--- src/functions/web/web.test.ts
+++ src/functions/web/web.test.ts
@@ -1,16 +1,4 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { gitHubRepoHomepageRegex } from './web';
 
-describe('PublicWeb', () => {
-	describe('GitHub url', () => {
-		it('should match on GitHub repository homepage', async () => {
-			let url = 'https://github.com/kyrolabs/awesome-agents';
-			expect(gitHubRepoHomepageRegex.test(url)).to.be.true;
-			url = 'https://github.com/kyrolabs/awesome-agents/';
-			expect(gitHubRepoHomepageRegex.test(url)).to.be.true;
-			url = 'https://github.com/kyrolabs/awesome-agents/blobs/master/README.md';
-			expect(gitHubRepoHomepageRegex.test(url)).to.be.false;
-		});
-	});
-});
+describe('PublicWeb', () => {});
