5a481a1679a0905389720e59e9ad3e5a0628fc7b
Daniel Campagnoli <daniel.campagnoli@trafficguard.ai>
2025-01-07 16:03:35 +0800

    Update llm route to handle invalid llm id's

--- src/routes/llms/llm-routes.ts
+++ src/routes/llms/llm-routes.ts
@@ -1,5 +1,6 @@
 import { send } from '#fastify/index';
 import { getLLM, llmTypes } from '#llm/llmFactory';
+import { logger } from '#o11y/logger';
 import { AppFastifyInstance } from '../../server';
 
 const basePath = '/api/llms';
@@ -8,8 +9,15 @@ export async function llmRoutes(fastify: AppFastifyInstance) {
 	// Returns the LLMs which are configured for the current user
 	fastify.get(`${basePath}/list`, async (req, reply) => {
 		const configuredLLMs = llmTypes()
-			.map((llm) => getLLM(llm.id))
-			.filter((llm) => llm.isConfigured())
+			.map((llm) => {
+				try {
+					return getLLM(llm.id);
+				} catch (e: any) {
+					logger.warn((e as Error).message);
+					return null;
+				}
+			})
+			.filter((llm) => llm?.isConfigured())
 			.map((llm) => ({ id: llm.getId(), name: llm.getDisplayName(), isConfigured: true }));
 		send(reply, 200, configuredLLMs);
 	});
